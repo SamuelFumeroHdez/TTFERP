aabe3369f4f2940f5c0d28c1d24fc3278da4def3
SamuelFumeroHdez <samuelfumerohdez@gmail.com>
2023-10-07 21:02:16 +0200

    First commit

---
+++ src/main/java/com/ttf/tallertornofumeroerp/TallertornofumeroerpApplication.java
@@ -0,0 +1,13 @@
+package com.ttf.tallertornofumeroerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TallertornofumeroerpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TallertornofumeroerpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ttf/tallertornofumeroerp/TallertornofumeroerpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ttf.tallertornofumeroerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallertornofumeroerpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
